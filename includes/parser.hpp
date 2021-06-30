fc82d6516c5b6abb2d3bf9889053666ec015a2bc
mTvare6 <mTvare6@users.noreply.github.com>
2021-06-30 21:34:34 +0530

    refactor: implement primitive parser, remove regex

--- includes/parser.hpp
+++ includes/parser.hpp
@@ -25,21 +25,9 @@ inline std::string reg_parse(std::string text) {
   std::regex ITALIC(R"(\*(.+)\*)");
   std::regex STRIKETHROUGH(R"(~~(.+)~~)");
   std::regex IMAGE_SIZE(R"(!\[(.+)\]\((.+) (.+) =(.+)x(.+)\))");
-  std::regex ABBREVIATIONS(R"(\*\[(.+)\]:\s(.+))");
+  std::regex ABBREVIATIONS(R"(\[(.+)\]:\s(.+))");
   std::regex UNDERLINE(R"(_(.+)_)");
   std::regex SPOILER(R"(!!(.+)!!)");
-  std::regex HEADING_1(R"(#\s(.+))");
-  std::regex HEADING_2(R"(##\s(.+))");
-  std::regex HEADING_3(R"(###\s(.+))");
-  std::regex HEADING_4(R"(####\s(.+))");
-  std::regex HEADING_5(R"(#####\s(.+))");
-  std::regex HEADING_6(R"(######\s(.+))");
-  text = regex_replace(text, HEADING_6, "<h6>$1</h6>");
-  text = regex_replace(text, HEADING_5, "<h5>$1</h5>");
-  text = regex_replace(text, HEADING_4, "<h4>$1</h4>");
-  text = regex_replace(text, HEADING_3, "<h3>$1</h3>");
-  text = regex_replace(text, HEADING_2, "<h2>$1</h2>");
-  text = regex_replace(text, HEADING_1, "<h1>$1</h1>");
   text = regex_replace(text, SPOILER, "<span class=\"spoiler\">$1</span>");
   text = regex_replace(text, UNDERLINE, "<u>$1</u>");
   text = regex_replace(text, ABBREVIATIONS, "<abbr title=\"$1\">$2</abbr>");
@@ -52,10 +40,15 @@ inline std::string reg_parse(std::string text) {
   return text;
 }
 
+struct MODE{
+  bool COLLAPSIBLE=false;
+  bool CUSTOM_CLASS=false;
+  bool BLOCKQUOTE_FENCED=false;
+};
+
 inline std::string fence_parse(std::string text) {
   std::string return_buf;
-  bool COLLAPSIBLE_special_mode;
-  bool CUSTOM_CLASS_special_mode;
+  MODE special_state;
 
   size_t start = 0;
   size_t end;
@@ -70,27 +63,72 @@ inline std::string fence_parse(std::string text) {
     }
 
     this_line = text.substr(start, end - start);
+    int line_siz = this_line.size()-1;
     if ((this_line.rfind("+++", 0) == 0)){
-      if(COLLAPSIBLE_special_mode){
+      if(special_state.COLLAPSIBLE){
         return_buf+="</details>\n";
       }
       else{
-        return_buf+="<details><summary>"+this_line.substr(4, this_line.size()-1)+"</summary>\n";
-        COLLAPSIBLE_special_mode=true;
+        return_buf+="<details><summary>"+this_line.substr(4, line_siz)+"</summary>\n";
+        special_state.COLLAPSIBLE=true;
       }
     }
     else if ((this_line.rfind(":::", 0) == 0)){
-      if(COLLAPSIBLE_special_mode){
+      if(special_state.CUSTOM_CLASS){
         return_buf+="</div>\n";
       }
       else{
-        return_buf+="<div class=\""+this_line.substr(4, this_line.size()-1)+"\">\n";
-        COLLAPSIBLE_special_mode=true;
+        return_buf+="<div class=\""+this_line.substr(4, line_siz)+"\">\n";
+        special_state.CUSTOM_CLASS=true;
       }
     }
 
+    else if ((this_line.rfind("<<<", 0) == 0)){
+      if(special_state.BLOCKQUOTE_FENCED){
+        return_buf+="</blockquote>\n";
+      }
+      else{
+        /* Add some code to include citation
+         * <blockquote cite="wikipedia.org/en/apple"> 
+         * apple tastes nice btw
+         * </blockquote>
+         * from
+         * <<< wikipedia.org/en/apple
+         * apple tastes nice btw
+         * <<<
+        */
+
+        return_buf+="<blockquote>\n";
+        special_state.BLOCKQUOTE_FENCED = true;
+      }
+      //special_state.BLOCKQUOTE_FENCED=!special_state.BLOCKQUOTE_FENCED;
+    }
+    else if ((this_line.rfind("###### ", 0) == 0)){
+      return_buf+="<h6>"+this_line.substr(7, line_siz)+"</h6>";
+    }
+    else if ((this_line.rfind("##### ", 0) == 0)){
+      return_buf+="<h5>"+this_line.substr(6, line_siz)+"</h5>";
+    }
+
+    else if ((this_line.rfind("#### ", 0) == 0)){
+      return_buf+="<h4>"+this_line.substr(4, line_siz)+"</h4>";
+    }
+    else if ((this_line.rfind("### ", 0) == 0)){
+      return_buf+="<h3>"+this_line.substr(3, line_siz)+"</h3>";
+    }
+
+    else if ((this_line.rfind("## ", 0) == 0)){
+      return_buf+="<h2>"+this_line.substr(2, line_siz)+"</h2>";
+    }
+    else if ((this_line.rfind("# ", 0) == 0)){
+      return_buf+="<h1>"+this_line.substr(1, line_siz)+"</h1>";
+    }
+    else if ((this_line.rfind("> ", 0) == 0)){
+      return_buf+="<blockquote>"+this_line.substr(1, line_siz)+"</blockquote>";
+    }
+
     else{
-    return_buf+=this_line+"\n";
+      return_buf+=this_line+"\n";
     }
     start = end + 1;
   }
@@ -98,5 +136,5 @@ inline std::string fence_parse(std::string text) {
 }
 inline std::string main_parse(const char *filename) {
   std::string file_c = readfile(filename);
-  return reg_parse(fence_parse(file_c));
+  return fence_parse(file_c);
 }
