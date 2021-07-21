9d727c754202e1dd6bddeae468e005eb3130b0b4
mTvare6 <mTvare6@users.noreply.github.com>
2021-07-21 14:14:00 +0530

    style: use a single function to parse inline types

--- src/parser.hpp
+++ src/parser.hpp
@@ -2,6 +2,29 @@
 #define PARSER_HPP_
 
 #include <string>
+
+struct ActiveState{
+  bool COLLAPSIBLE=false;
+  bool CUSTOM_CLASS=false;
+  bool BLOCKQUOTE_FENCED=false;
+  bool CODE_FENCED=false;
+  bool UNORDERED_LIST=false;
+
+  bool BOLD=false;
+  bool ITALIC=false;
+  bool UNDERLINE=false;
+  bool STRIKETHROUGH=false;
+  bool SPOILER=false;
+
+  bool SUBSCRIPT=false;
+  bool SUPERSCRIPT=false;
+
+  bool CODE_INLINE=false;
+};
+
+
+
+void LineConverter(std::string *buf, std::string line, ActiveState *as, uint8_t startIndex, bool *no_exp);
 std::string FenceConverter(const std::string *text);
 #endif
 
